2145fb6269283e2b81fbac42a5b3b6d41816354d
MOHAMED NIHAJ K <mohamed19130391@ssmpoly.ac.in>
2022-01-28 20:01:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,10 +14,11 @@ df = pd.read_csv('dataset.csv')
 app.layout = html.Div([
     dcc.DatePickerSingle(
         id='date-picker',
+        month_format='MMM Do, YY',
         min_date_allowed=date(2017, 1, 1),
-        max_date_allowed=date(2020, 10, 7),
-        initial_visible_month=date(2020, 8, 5),
-        date=date(2020, 8, 5)
+        max_date_allowed=date(2020, 7, 10),
+        initial_visible_month=date(2020, 7, 10),
+        date=date(2020, 6, 5)
     ),
     dcc.Graph(id='graph-with-slider'),
     dcc.RangeSlider(
@@ -33,26 +34,24 @@ app.layout = html.Div([
 @app.callback(
     Output('graph-with-slider', 'figure'),
     Input('time-slider', 'value'),
-    Input('date-picker', 'value')
+    Input('date-picker', 'date')
 )
 def update_figure(time_slider,date_picker):
-    print(date_picker)
-    if date_picker is not None:
-        date_object = date.fromisoformat(date_picker)
-        date_string = date_object.strftime('%B %d, %Y')
-        print(string_prefix + date_string)
+    #print(date_picker)
     rn1 = time_slider[0]
     rn2 = time_slider[1]
-    if rn1 < 10:
-        rn1 = '0' + str(rn1)
-    if rn2 < 10:
-        rn2 = '0' + str(rn2)
-
-    rng1 = "2022-01-15 " +str(rn1)+ ":00:00"
-    rng2 = "2022-01-15 " + str(rn2) + ":00:00"
-    
+    if date_picker is not None:
+        date_object = date.fromisoformat(date_picker)
+        print(date_object)
+        date_string = date_object.strftime("%-m/%-d/%Y")  
+    date_string = date_string.replace(',','/')
+    date_string = date_string.replace(" ","")
+    rng1 = str(date_string)+ ' ' + str(rn1)+ ":00"
+    print(rng1)
+    rng2 =str(date_string)+ ' ' + str(rn2) + ":00"
+    print(rng2)
     dff = df[(rng1 <= df['time']) & (df['time'] <= rng2)]
-    #print(dff)
+    print(dff)
     fig = px.bar(x=dff['time'], y=dff['renewablespercentage'])
     fig.update_xaxes(type='category')
     fig.update_layout()
